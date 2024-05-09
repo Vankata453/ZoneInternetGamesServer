82452cb336147c167e63cf6963feff12454bcdf5
Vankata453 <78196474+Vankata453@users.noreply.github.com>
2024-05-10 00:31:34 +0300

    Client DLL: Do not `new` `WCHAR` pointers

--- InternetGamesClientDLL/Dialogs.cpp
+++ InternetGamesClientDLL/Dialogs.cpp
@@ -23,18 +23,16 @@ INT_PTR CALLBACK ProcessMessage(HWND hDialog, UINT message, WPARAM wParam, LPARA
                 case ID_SET:
                 {
                     // Host string
-                    LPWSTR wHost = new WCHAR[256];
+                    WCHAR wHost[256];
                     if (!GetDlgItemTextW(hDialog, IDC_HOST, wHost, 256))
                         printf("Couldn't get specified remote address host in remote address dialog.\n");
                     valHost = wHost;
-                    delete[] wHost;
 
                     // Port string
-                    LPWSTR wPort = new WCHAR[5];
+                    WCHAR wPort[5];
                     if (!GetDlgItemTextW(hDialog, IDC_PORT, wPort, 5))
                         printf("Couldn't get specified remote address port in remote address dialog.\n");
                     valPort = wPort;
-                    delete[] wPort;
 
                     // Do not close the dialog, if a host and port aren't specified
                     if (valHost.empty() || valPort.empty())
