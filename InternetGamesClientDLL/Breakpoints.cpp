3f79290d7c5918b47c242723df1a1eb800856ee5
Vankata453 <78196474+Vankata453@users.noreply.github.com>
2024-05-11 01:51:53 +0300

    Client DLL: Support for the x86-32 `WOW64_CONTEXT`
    The `GetParamsFromContext()` function is now used to get an array of the parameters, passed to a function, from a `CONTEXT` pointer, also supporting x86-32, where `WOW64_CONTEXT` is used instead.
    
    This fixes x86 compilation of the Internet Games Client DLL project.

--- InternetGamesClientDLL/Breakpoints.cpp
+++ InternetGamesClientDLL/Breakpoints.cpp
@@ -3,6 +3,7 @@
 #include "Breakpoints.hpp"
 
 #include <stdio.h>
+#include <vector>
 #include <ws2def.h>
 
 #include "Dialogs.hpp"
@@ -39,6 +40,22 @@ bool SetBreakpoints()
 }
 
 
+// Helper function for getting function parameters from CONTEXT (x86-64) or WOW64_CONTEXT (x86-32).
+// Based on https://devblogs.microsoft.com/oldnewthing/20230321-00/?p=107954
+#if _WIN64
+std::vector<DWORD64> GetParamsFromContext(const CONTEXT* c)
+{
+    return { c->Rcx, c->Rdx, c->R8, c->R9,
+        c->Rsp + 32, c->Rsp + 40, c->Rsp + 48, c->Rsp + 56, c->Rsp + 64 };
+}
+#else
+std::vector<DWORD> GetParamsFromContext(const CONTEXT* c)
+{
+    return { c->Esp + 4, c->Esp + 8, c->Esp + 12, c->Esp + 16,
+        c->Esp + 20, c->Esp + 24, c->Esp + 28, c->Esp + 32, c->Esp + 36 };
+}
+#endif
+
 /* Exception handler */
 LONG CALLBACK VectoredExceptionHandler(EXCEPTION_POINTERS* exceptionInfo)
 {
@@ -48,18 +65,19 @@ LONG CALLBACK VectoredExceptionHandler(EXCEPTION_POINTERS* exceptionInfo)
 
         const DWORD_PTR exceptionAddress = reinterpret_cast<DWORD_PTR>(exceptionInfo->ExceptionRecord->ExceptionAddress);
         CONTEXT* context = exceptionInfo->ContextRecord;
+        const auto params = GetParamsFromContext(context);
 
         // Perform actions, based on the function
         if (exceptionAddress == reinterpret_cast<DWORD_PTR>(GetAddrInfoW))
         {
-            if (!wcscmp(reinterpret_cast<PCWSTR>(context->Rcx), L"mpmatch.games.msn.com")) // Ensure the function is trying to connect to the default game server
+            if (!wcscmp(reinterpret_cast<PCWSTR>(params[0]), L"mpmatch.games.msn.com")) // Ensure the function is trying to connect to the default game server
             {
                 // Set custom address
-                wcscpy(reinterpret_cast<WCHAR*>(context->Rcx), RemoteAddressDialog::valHost.c_str());
-                wcscpy(reinterpret_cast<WCHAR*>(context->Rdx), RemoteAddressDialog::valPort.c_str());
+                wcscpy(reinterpret_cast<WCHAR*>(params[0]), RemoteAddressDialog::valHost.c_str());
+                wcscpy(reinterpret_cast<WCHAR*>(params[1]), RemoteAddressDialog::valPort.c_str());
 
                 // Set properties, required for connecting to the custom server
-                ADDRINFOW* addrInfo = reinterpret_cast<ADDRINFOW*>(context->R8);
+                ADDRINFOW* addrInfo = reinterpret_cast<ADDRINFOW*>(params[2]);
                 addrInfo->ai_family = AF_INET;
                 addrInfo->ai_protocol = IPPROTO_TCP;
             }
